c2446ddc2400a0dc4bd5f143459c69f962bca70f
StepanchukYI <bodunjo855@gmail.com>
2017-07-28 14:43:30 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Some Task</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+</head>
+<body>
+<!----------------------------------------------------SHAPKA--------------------------------------------------------------->
+<div class="navbar navbar-default navbar-static-top">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-4">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#responsive-menu">
+                        <span class="sr-only">Open navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Logo</a>
+                </div>
+            </div>
+            <div class="col-xs-8">
+                <div class="collapse navbar-collapse" id="responsive-menu">
+                    <ul class="nav navbar-nav">
+                        <li><a href="#">Home</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Product<b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Smthng 1</a></li>
+                                <li><a href="#">Smthng 2</a></li>
+                                <li><a href="#">Smthng 3</a></li>
+                                <li><a href="#">Smthng 4</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">FAQ</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<!----------------------------------------------------CONTENT--------------------------------------------------------------->
+<div class="container">
+    <div class="row">
+        <div class="col-xs-2">
+            <ul class="left-menu">
+                <li><a href="">Cats</a></li>
+                <li><a href="">Cows</a></li>
+                <li><a href="">Cacao</a></li>
+                <li><a href="">Coca-cola</a></li>
+                <li><a href="">Cool potato</a></li>
+                <li><a href="">Cars</a></li>
+            </ul>
+        </div>
+        <div class="col-xs-10">
+            <h3>Content</h3>
+            <p>Up maids me an ample stood given. Certainty say suffering his him collected intention promotion. Hill
+                sold ham men made lose case. Views abode law heard jokes too. Was are delightful solicitude discovered
+                collecting man day. Resolving neglected sir tolerably but existence conveying for. Day his put off
+                unaffected literature partiality inhabiting.
+
+                Affronting imprudence do he he everything. Sex lasted dinner wanted indeed wished out law. Far advanced
+                settling say finished raillery. Offered chiefly farther of my no colonel shyness. Such on help ye some
+                door if in. Laughter proposal laughing any son law consider. Needed except up piqued an.
+
+                Much did had call new drew that kept. Limits expect wonder law she. Now has you views woman noisy match
+                money rooms. To up remark it eldest length oh passed. Off because yet mistake feeling has men. Consulted
+                disposing to moonlight ye extremity. Engage piqued in on coming.
+
+                Stronger unpacked felicity to of mistaken. Fanny at wrong table ye in. Be on easily cannot innate in
+                lasted months on. Differed and and felicity steepest mrs age outweigh. Opinions learning likewise
+                daughter now age outweigh. Raptures stanhill my greatest mistaken or exercise he on although. Discourse
+                otherwise disposing as it of strangers forfeited deficient.
+
+                On am we offices expense thought. Its hence ten smile age means. Seven chief sight far point any. Of so
+                high into easy. Dashwoods eagerness oh extensive as discourse sportsman frankness. Husbands see disposed
+                surprise likewise humoured yet pleasure. Fifteen no inquiry cordial so resolve garrets as. Impression
+                was estimating surrounded solicitude indulgence son shy.
+
+                Consider now provided laughter boy landlord dashwood. Often voice and the spoke. No shewing fertile
+                village equally prepare up females as an. That do an case an what plan hour of paid. Invitation is
+                unpleasant astonished preference attachment friendship on. Did sentiments increasing particular nay. Mr
+                he recurred received prospect in. Wishing cheered parlors adapted am at amongst matters.
+
+                Smile spoke total few great had never their too. Amongst moments do in arrived at my replied. Fat
+                weddings servants but man believed prospect. Companions understood is as especially pianoforte
+                connection introduced. Nay newspaper can sportsman are admitting gentleman belonging his. Is oppose no
+                he summer lovers twenty in. Not his difficulty boisterous surrounded bed. Seems folly if in given scale.
+                Sex contented dependent conveying advantage can use.
+
+                Far quitting dwelling graceful the likewise received building. An fact so to that show am shed sold
+                cold. Unaffected remarkably get yet introduced excellence terminated led. Result either design saw she
+                esteem and. On ashamed no inhabit ferrars it ye besides resolve. Own judgment directly few trifling.
+                Elderly as pursuit at regular do parlors. Rank what has into fond she.
+
+                Tolerably earnestly middleton extremely distrusts she boy now not. Add and offered prepare how cordial
+                two promise. Greatly who affixed suppose but enquire compact prepare all put. Added forth chief trees
+                but rooms think may. Wicket do manner others seemed enable rather in. Excellent own discovery unfeeling
+                sweetness questions the gentleman. Chapter shyness matters mr parlors if mention thought.
+
+                Agreed joy vanity regret met may ladies oppose who. Mile fail as left as hard eyes. Meet made call in
+                mean four year it to. Prospect so branched wondered sensible of up. For gay consisted resolving
+                pronounce sportsman saw discovery not. Northward or household as conveying we earnestly believing. No in
+                up contrasted discretion inhabiting excellence. Entreaties we collecting unpleasant at everything
+                conviction.
+
+            </p>
+        </div>
+    </div>
+</div>
+<!----------------------------------------------------FOOTER--------------------------------------------------------------->
+
+<div class="container">
+    <hr>
+    <div class="row">
+        <div class="col-xs-3">
+            <ul class="list-unstyled">
+                <li>GitHub
+                <li>
+                <li><a href="#">About us</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Contact & support</a></li>
+                <li><a href="#">Enterprise</a></li>
+                <li><a href="#">Site status</a></li>
+            </ul>
+        </div>
+        <div class="col-xs-3">
+            <ul class="list-unstyled">
+                <li>Applications
+                <li>
+                <li><a href="#">Product for Mac</a></li>
+                <li><a href="#">Product for Windows</a></li>
+                <li><a href="#">Product for Eclipse</a></li>
+                <li><a href="#">Product mobile apps</a></li>
+            </ul>
+        </div>
+        <div class="col-xs-3">
+            <ul class="list-unstyled">
+                <li>Services
+                <li>
+                <li><a href="#">Web analytics</a></li>
+                <li><a href="#">Presentations</a></li>
+                <li><a href="#">Code snippets</a></li>
+                <li><a href="#">Job board</a></li>
+            </ul>
+        </div>
+        <div class="col-xs-3">
+            <ul class="list-unstyled">
+                <li>Documentation
+                <li>
+                <li><a href="#">Product Help</a></li>
+                <li><a href="#">Developer API</a></li>
+                <li><a href="#">Product Markdown</a></li>
+                <li><a href="#">Product Pages</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.js"></script>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
